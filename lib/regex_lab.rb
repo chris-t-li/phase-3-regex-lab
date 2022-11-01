17e26eaa3e8ee6a69a5f1d8187e1d4e73565d839
Chris Li <chris.lty07@gmail.com>
2022-11-01 13:13:32 -0400

    commit tree d52eff3f2eac6334cb01d2c74a92d03654951f59

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,14 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
+def starts_with_a_vowel?(word)
+    binding.pry
+    word.each do |w|
+        w.match(/^[aeiou]\w+/) ? true : false
+    end
+    # binding.pry
 end
+
+# p starts_with_a_vowel?(%w{ afoot Excellent incredible Absolute unreal Inconceivable octopus })
 
 def words_starting_with_un_and_ending_with_ing(text)
 
